37d84b34f9775f91884f610e22ec8430681d496f
manyyeon <udayeon@naver.com>
2022-08-20 14:38:10 +0900

    사용 안하는 거 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HouseDetail from 'pages/Houses/HouseDetail';
 import HouseList from 'pages/Houses/HouseList';
 
